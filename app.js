2e3127ccc29d80f7cd72af6b63ed6b338d242489
Andrés Aparicio <andres.apa.23@gmail.com>
2023-11-28 09:54:15 -0500

    Update routes paths in app.js and bin/www

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
-var jobsRouter = require('./routes/jobs')
+var indexRouter = require('./src/routes/index')
+var usersRouter = require('./src/routes/users')
+var jobsRouter = require('./src/routes/jobs')
 
 var app = express()
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,8 +2,12 @@ var express = require('express')
 var router = express.Router()
 
 /* GET - get user info */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
+router.get('/login/', function (req, res, next) {
+  if (req.body?.email) {
+    res.status(200).send('email provided')
+  } else {
+    res.status(400).send('no email provided')
+  }
 })
 
 /* POST - create new user */
