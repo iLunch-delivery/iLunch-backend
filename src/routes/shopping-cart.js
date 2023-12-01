f106284ae75eec4b2e963261932517be629becba
Jose <80296654+Dajovih@users.noreply.github.com>
2023-12-01 01:44:23 -0500

    feat: Changes

--- src/routes/shopping-cart.js
+++ src/routes/shopping-cart.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const Cart = require('../models/ordersCart.model')
-const Products = require('../models/ordersProducts.model')
 
 
 const {
@@ -12,17 +10,6 @@ const {
 } = require('../controllers/shoppin-cart.controller')
 
 //GET - get shopping cart elements
-router.get('/:userId', (req, res) => {
-    if (req.params?.userId){
-        try {
-            const {userId} = req.params
-            const cart = await OrderCart.findOne({ userId: Number(restaurantId) });
-            const 
-        } 
-        catch {
-
-        }
-    }
-})
+router.get('/:userId', getShoppingCart)
 
 module.exports = router;
